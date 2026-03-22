206f7aa0b4ce490563b6853c823d456335fedf9e
jaswanth <jaswanth@gmail.com>
2026-03-22 19:52:54 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    port: 80,
+    host: true, // <--- Add this! This exposes the server to the internet
+    strictPort: true,
+  },
   plugins: [react(), tailwindcss()],
 })
